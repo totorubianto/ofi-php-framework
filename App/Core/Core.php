aceda60adc7978767f0e6740980804215c811c3c
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-20 16:34:07 +0700

    Percobaan controller dua

--- App/Core/Core.php
+++ App/Core/Core.php
@@ -24,56 +24,6 @@ class Core
         return null;
      }
 
-    public function getControllerActionParam()
-    {       
-
-        if (!empty($this->Url) && $this->Url != '/')
-        {
-            $this->Url = explode('/', $this->Url);
-            array_shift($this->Url);
-            $this->Controller = $this->Url[0] . 'Controller';
-            array_shift($this->Url);
-        }
-        else
-        {
-            $this->Url = array();
-            $this->Controller = 'indexController';
-        }
-
-        $this->getAction();
-        $this->getParams();
-    }
-    public function getAction()
-    {
-        $this->Action = 'index';
-        if (isset($this->Url[0]) && !empty($this->Url[0]))
-        {
-            $this->Action = $this->Url[0];
-        }
-
-    }
-
-    public function getParams()
-    {
-        if (count($this->Url) > 0)
-        {
-            $this->Params = $this->Url;
-        }
-    }
-    public function execute()
-    {
-        // $className = '\\APP\\Controllers\\' . $this->Controller;
-        // if (!class_exists($className))
-        // {
-        //     (new \App\Controllers\errorController())->index();
-        // }
-        // $classNameController = new $className();
-        // call_user_func_array(array(
-        //     $classNameController,
-        //     $this->Action
-        // ) , $this->Params);
-    }
-
     public function route()
     {
         include 'App\route\web.php';
@@ -119,20 +69,15 @@ class Core
             } elseif ($searchValue['type'] == 'controller') {
 
                 // Jika type controller maka memanggil controller
-
+                // @ untuk memanggil methodnya
+                
                 $request_controller = explode('@', $searchValue['to']);
 
                 $get_only_Controller_Name = $request_controller[0];
                 $get_only_Method_Name = $request_controller[1];
 
                 $className = '\\APP\\Controllers\\' . $get_only_Controller_Name;
-
-                if (!class_exists($className)) {
-                    $controller->error500("Controller " . $className . ' not found please recorrect again');
-                }
-
                 $classNameController = new $className();
-
                 $classNameController->$get_only_Method_Name();
 
             }
