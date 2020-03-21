cc6cddbf7c996aec97117c59f83c8058ab56c917
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-21 22:32:36 +0700

    Menyederhanakan file config

--- config.php
+++ config.php
@@ -12,13 +12,3 @@ $config['password']='';
 define('PROJECTNAME', 'OFI Framework');
 define('ENVIRONMENT', 'development');
 
-// Dont change this line
-define('BASE', (
-    isset($_SERVER['HTTPS']) && 
-    $_SERVER['HTTPS'] === 'on' ? "https" : "http") . 
-    "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-
-define('BASEURL', __DIR__ . '/');
-define('PROJECTPATH', __DIR__);
-header('Access-Control-Allow-Origin: *');
-
