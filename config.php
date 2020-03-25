1b0bb7263cd405af131150a99cf915d8ccdd6a70
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-26 05:48:35 +0700

    Menambah pengaturan SEO web

--- config.php
+++ config.php
@@ -12,3 +12,25 @@ $config['password']='';
 define('PROJECTNAME', 'OFI Framework');
 define('ENVIRONMENT', 'development');
 
+/**
+ * Define SEO tag for your website
+ * please change according to your needs
+ * 
+ * What is google-site-verification?
+ * For example for the tag :
+ * <meta name="google-site-verification" content="ZJTLoB1wuXx1aV_gw0ATcBmS0tk8M3IuUkOMi_Qi6C8" />
+ * 
+ * You can only take the meta tag content, and then the content is
+ *          ZJTLoB1wuXx1aV_gw0ATcBmS0tk8M3IuUkOMi_Qi6C8 
+ * please put thats content in define('GoogleSiteVerification', '');
+ */
+
+// Description for your website
+define('DESCRIPTION', 'OFI PHP Framework');
+define('AUTHOR', 'Teguh Rijanandi');
+
+// Separate with comma
+define('KEYWORDS', 'Framework, PHP, Backend'); 
+define('GoogleSiteVerification', '');
+
+
--- vendor/template.php
+++ vendor/template.php
@@ -3,10 +3,18 @@
 <head>
 	<title> <?php echo PROJECTNAME ?> </title>
 	<meta charset="utf-8">
-	<meta name="description" content="sangat mempesona banyumas" >
-	<meta  name="key" content="google">
+	<meta name="description" content="<?php echo DESCRIPTION ?>" >
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:title" content="The Rock" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="<?php echo BASE; ?>" />
+	<link rel="shortcut icon" href="favicon.png">
+	<meta property="og:image" content="favicon.png" />
+	<meta name="robots" content="index, follow">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="<?php echo AUTHOR ?>">
+	<meta name="google-site-verification" content="<?php echo GoogleSiteVerification ?>" />
 	
 		<link rel="stylesheet" type="text/css" href=" /assets/css/bootstrap.min.css">
 </head>
