422870ff300c07a83e98a65fd90a8b3c187e2a32
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-27 07:13:02 +0700

    Bug fixed

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -16,7 +16,8 @@ class Model
     {   
         global $config;
         $koneksi = new mysqli($config['host'], $config['user'], $config['password'], $config['dbname']);
-        return $koneksi->query($sql);
+        $hasil = $koneksi->query($sql);
+        return $hasil->fetch_all(MYSQLI_ASSOC);
     }
 
     public static function count($sql)
@@ -27,11 +28,6 @@ class Model
     public function fetch($sql)
     {
         return mysqli_fetch_assoc($sql);
-    }
-
-    public function fetchRow($sql)
-    {
-        return mysqli_fetch_all($sql);
     }
 }
 
