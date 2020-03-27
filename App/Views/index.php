0db591cf343ce7bdc670bdc3b51e54698532491a
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-27 23:02:32 +0700

    Testing make a form Group with PHP

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,37 +1,14 @@
 <?php 
-    // Default all required input
-    $design->form('post', [
-        'url' => '#', // Up to you Where you want to post data
-        'submit' => 'Simpan', // value of submit button
-        'input' => [ // contains ofi php framework input script
-            [
-                'name' => 'Email',
-                'type' => 'email', // null, text, password, email and etc
-                'required' => 'no', // optional, you can remove this line
-            ],
-
-            [
-                'name' => 'Jenis Kelamin', // jenis kelamin (gender)
-                'input' => 'select',
-                'required' => 'yes',
-                'option' => [ // option can be use in select input only
-                    [
-                        'value' => 'l', // value of option tag
-                        'text' => "laki laki" // text in option tag
-                    ],
-                    [
-                        'value' => 'p',
-                        'text' => "Perempuan",
-                    ]
-                ]
-            ],
-
-            [
-                'name' => 'alamat',
-                'input' => 'textarea', // input, textarea, select
-                'required' => 'no', // yes or no
-            ]
-        ],
+    $design->collapse([
+        'name' => "Collapseku", // must same
+        'type' => 'link', // btn or collapse
+        'value' => "Toggle Collapse"
     ]);
-    
+
+    // You can put this script where do you want
+    $design->collapse([
+        'name' => "Collapseku", // must same
+        'type' => 'collapse',
+        'value' => "My first collapse with php function"
+    ]);
 ?>
