b5a34ee0cfc659065d6b34cb71e5a4ebc5fb5b92
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-03-27 14:31:18 +0700

    Penyederhanaan

--- index.php
+++ index.php
@@ -21,9 +21,6 @@ define('BASE', (
     isset($_SERVER['HTTPS']) && 
     $_SERVER['HTTPS'] === 'on' ? "https" : "http") . 
     "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-
-define('BASEURL', __DIR__ . '/');
-define('PROJECTPATH', __DIR__);
 header('Access-Control-Allow-Origin: *');
 
 /**
